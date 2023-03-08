b3fe9aa11c6e66f1953acee7b54bd820318e3784
Edsonjsf <edsonjsf1990@gmail.com>
2023-03-08 14:36:11 -0400

    Menú de la aplicación

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  path: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class MenuComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class MenuComponent {
+  menus: MenuItem[] = [
+    { path: '/graficas/barra', name: 'Barras' },
+    { path: '/graficas/barra-doble', name: 'Barras Doble' },
+    { path: '/graficas/dona', name: 'Dona' },
+    { path: '/graficas/dona-http', name: 'Dona Http' },
+  ];
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { MenuComponent } from './components/menu/menu.component';
 
 const importExport = [MenuComponent];
 
 @NgModule({
   declarations: [importExport],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [importExport],
 })
 export class SharedModule {}
