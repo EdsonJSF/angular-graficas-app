6bfd80b087958c9cf1ac79689fa23ea293a60abc
Edsonjsf <edsonjsf1990@gmail.com>
2023-03-08 14:12:24 -0400

    Shared Module, Rutas y LazyLoad

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
 
+const importExport = [MenuComponent];
+
 @NgModule({
-  declarations: [MenuComponent],
+  declarations: [importExport],
   imports: [CommonModule],
+  exports: [importExport],
 })
 export class SharedModule {}
