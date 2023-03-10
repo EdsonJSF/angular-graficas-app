cc05c3b48f3b43ac25112d48d7198231f47c9cb5
Edsonjsf <edsonjsf1990@gmail.com>
2023-03-10 13:54:02 -0400

    Componente personalizado para mostrar gráficos

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -6,7 +9,87 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class GraficaBarraComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+    },
+  };
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        hoverBackgroundColor: '#EF3B35',
+        backgroundColor: '#EF3B35BB',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+        hoverBackgroundColor: '#3A43E0',
+        backgroundColor: '#3A43E0BB',
+      },
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series C',
+        hoverBackgroundColor: '#39F04C',
+        backgroundColor: '#39F04CBB',
+      },
+    ],
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    this.barChartData.datasets.forEach((serie) => {
+      serie.data = [
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+      ];
+    });
+
+    this.chart?.update();
+  }
 }
